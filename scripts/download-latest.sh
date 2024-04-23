d4a7ce73d693ec5152d1b440a52ab50946c77e13
zekroTJA <riho@live.de>
2024-04-23 11:08:39 +0200

    add download debug information

--- scripts/download-latest.sh
+++ scripts/download-latest.sh
@@ -4,8 +4,17 @@ APP_ID="715572"
 
 latest_id=$(curl -Ls "https://www.curseforge.com/api/v1/mods/$APP_ID/files?pageIndex=0&pageSize=1&sort=dateCreated&sortDescending=true&removeAlphas=true" \
     | jq '.data[0].id')
+
+echo "-- Latest ID: $latest_id"
     
 server_id=$(curl -Ls "https://www.curseforge.com/api/v1/mods/$APP_ID/files/$latest_id/additional-files" \
     | jq '.data[0].id')
+
+echo "-- Server ID: $server_id"
+
+file_name=$(curl -Ls "https://www.curseforge.com/api/v1/mods/$APP_ID/files/$server_id" \
+    | jq -r '.data.fileName')
+
+echo "-- File Name: $file_name"    
 
 curl -Lo "$1" "https://www.curseforge.com/api/v1/mods/$APP_ID/files/$server_id/download"
