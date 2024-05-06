f3b69e7cdda65974b7c0b4f2d86d6a5c7bf084ae
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2024-05-06 15:39:51 +0200

    patch hotfix

--- scripts/patch-mods.sh
+++ scripts/patch-mods.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-mv patch/mods/pipez-forge-1.20.1-1.2.6.jar mods/pipez-*
+test -f patch/mods/pipez-forge-1.20.1-1.2.6.jar \
+    && mv patch/mods/pipez-forge-1.20.1-1.2.6.jar mods/pipez-*
